42d5e1b3b96d8d2171af356436b476b632c2e035
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-03-02 13:29:13 -0500

    added custom null pointer & generic exception handlers

--- src/main/java/com/colinven/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/colinven/blog/exception/GlobalExceptionHandler.java
@@ -14,7 +14,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<APIError> handleResourceNotFoundException(ResourceNotFoundException exception, HttpServletRequest request) {
-
         HttpStatus status = HttpStatus.NOT_FOUND;
         APIError error = new APIError(
                 LocalDateTime.now(),
@@ -25,5 +24,32 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<APIError> handleNullPointerException(NullPointerException exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        APIError error = new APIError(
+                LocalDateTime.now(),
+                status.value(),
+                exception.getMessage(),
+                "Something went wrong. Please try again later.",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIError> handleGenericException(Exception exception, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        APIError error = new APIError(
+                LocalDateTime.now(),
+                status.value(),
+                exception.getMessage(),
+                "Something went wrong. Please try again later.",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
